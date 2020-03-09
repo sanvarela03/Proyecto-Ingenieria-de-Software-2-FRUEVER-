41c7f19ca9a030a234369d7c7a953308fe45114e
Santiago Varela Daza <svarela03@uan.edu.co>
2020-03-09 16:22:19 -0500

    santiago varela - primer commit

---
+++ mercaleaf/src/main/java/mercado/MercaleafApplication.java
@@ -0,0 +1,13 @@
+package mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaleafApplication.class, args);
+	}
+
+}
---
+++ mercaleaf/src/test/java/mercado/MercaleafApplicationTests.java
@@ -0,0 +1,13 @@
+package mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
